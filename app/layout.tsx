39507e4ed8cfb866d7de0ce19eeb8ecc33933aab
Dũng Hoàng <dunghoang879@gmail.com>
2024-06-11 23:38:57 +0700

    start

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import { ModalProvider } from "@/providers/modal-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "MBA INTERNATIONAL",
+  description: "MBA INTERNATIONAL",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <ModalProvider>
+          </ModalProvider>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
